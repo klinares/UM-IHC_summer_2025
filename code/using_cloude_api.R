7e749609b6082c97d622b56930590d6701aa8c05
klinares <kev.a.linares@gmail.com>
2025-06-04 15:56:52 -0400

    adding llama code

--- code/using_cloude_api.R
+++ code/using_cloude_api.R
@@ -1,10 +1,9 @@
 library(ellmer)
-library(usethis)
 library(tidyverse)
 library(pdftools)
 
 # initialize connection after adding api to .Renvironment file
-## usethis::edit_r_environ() opens file
+## usethis::edit_r_environ() opens file, type 'ANTHROPIC_API_KEY="API_key" '
 
 client <- chat_anthropic(model = "claude-3-7-sonnet-latest")
 
@@ -14,42 +13,53 @@ client$chat("Summarize the plot of Romeo and Juliet in less than 50 words")
 # can be used w/ browser
 #live_browser(client)
 
+# where files are stored
+setwd("C:/Users/KLinares/Documents/UM-IHC_summer_2025/past_studies")
+
 system_prompt <- paste(
   "You are a graduate researcher and need to better understand the current body of research products in the lab",
-  "<research_study>",
-  pdf_text("InHypo-DM Qualitative - HCPs' Emotions - Diabetes Spectr_2021.pdf"),
-  "/research_study",
+  #"<research_study>",
+  #pdf_text("InHypo-DM Qualitative - HCPs' Emotions - Diabetes Spectr_2021.pdf"),
+  #"/research_study",
   "* Use examples when helpful.",
   collapse = "\n"
 )
+
+client_claude <- chat_anthropic(system_prompt, 
+                                model = "claude-3-7-sonnet-latest")
  
-client <- chat_anthropic(system_prompt, model = "claude-3-7-sonnet-latest")
-live_browser(client)
+# live_browser(client_claude)  is interactive
 
-# test 2 with data
-system_prompt <- paste(
-  "You are a graduate researcher that will identify and categorize emotional states expressed by participants, 
-  using a systematic approach.",
-  "* Use examples when helpful.",
-  collapse = "\n"
-)
+# we need to install locally ollama and the ollamar package. 
+## we can than install llama models using ollamar::pull(<model name>)
+### we can print which models are installed locally with 
+ollamar::list_models()
 
-client <- chat_anthropic(system_prompt, model = "claude-3-7-sonnet-latest")
+# llama4 and 3 are too large
+client_llama <- chat_ollama(system_prompt, model = "llama3.2")
+client_gemma <- chat_ollama(system_prompt = system_prompt , model = "gemma")
 
-
-results <- map(emotion_states, function(x){
-  
-  
-  
-})
-
-client$chat(emotion_states[[1]])
+# ask the LLM to do so something
+string_question <- "Summarize this article in few sentences with an empahsizes on research design."
 
 
+# read in pdf publication of a research paper
+article <- pdf_text("InHypo-DM Qualitative - HCPs' Emotions - Diabetes Spectr_2021.pdf")
 
-# using ollama
-library(rollama)
+# model claude sonnet 3.7
+client_claude$chat(string_question, article)
 
-rollama::ping_ollama()
+# model llama3
+client_llama$chat(string_question, article)
+
+# model gemma
+client_gemma$chat(string_question, article)
+
+
+
+
+
+
+
 
 
